bdc21f6091c248aa83e1d5c49b6a8ca6138630d8
GeorgianaIoana <victornedelcu90@gmail.com>
2023-11-26 23:16:51 +0200

    reactEx1

--- react-components/ex01/app.js
+++ react-components/ex01/app.js
@@ -33,7 +33,7 @@ const person = {
 };
 
 console.warn(`
-  Folosind metoda map pe arrayul skills, returneaza
+  1.Folosind metoda map pe arrayul skills, returneaza
   si afiseaza in consola un array in care fiecare
   consoana este scrisa cu litera mare (vocalele nu)
 `);
@@ -54,7 +54,7 @@ const arrVowels = person.skills.map((skill) => {
 console.log(arrVowels);
 
 console.warn(`
-  Folosind map pe arrayul friends, returneaza un array
+ 2. Folosind map pe arrayul friends, returneaza un array
   in care fiecare pozitie contine propozitia
   “Ma numesc {name} {surname} si am {age} ani.  ”
 `);
@@ -64,3 +64,90 @@ const sentences = person.friends.map((friend) => {
   return `Ma numesc ${name} ${surname} si am ${age} ani.`;
 });
 console.log(sentences);
+
+console.warn(`3. Folosind map pe arrayul friends, returneaza un array in care fiecare pozitie contine propozitia
+“Diferenta de varsta dintre {friendName} si {personName} este {diff}`);
+
+const difAge = person.friends.map((friend) => {
+  const { name: friendName, surname: friendSurname, age: friendAge } = friend;
+  const { name: personName, surname: personSurname, age: personAge } = person;
+  const diff = Math.abs(personAge - friendAge);
+  return `Diferenta de varsta dintre ${friendName} si ${personName} este de ${diff}.`;
+});
+console.log(difAge);
+
+console.warn(`4.Returneaza si afiseaza un array in care fiecare pozitie contine diferenta dintre varsta persoanei si
+ lungimea cuvantului de pe arrayul skill`);
+
+const diffAgeSkill = person.skills.map((skill) => {
+  const diff = person.age - skill.length;
+
+  return diff;
+});
+console.log(diffAgeSkill);
+
+console.warn(
+  `5.Folosind metoda map pe arrayul skills, returneaza un array care contine cuvintele cu prima si ultima litera mari.`,
+);
+
+const joy = person.skills.map((skill) => {
+  const skillLetters = skill.split('');
+  const firstAndLastLetter = skillLetters.map((letter, index, letters) => {
+    if (index === 0 || index === letters.length - 1) {
+      return letter.toUpperCase();
+    }
+    return letter;
+  });
+  return firstAndLastLetter.join('');
+});
+console.log(joy);
+
+console.warn(`6.Folosind metoda map pe arrayul skills, returneaza un array care contine cuvintele inversate (exemplu: lmth)
+`);
+const reverseWord = person.skills.map((skill) => {
+  return skill.split('').reverse().join('');
+});
+console.log(reverseWord);
+
+console.warn(`7.Folosind metoda map pe arrayul friends,
+returneaza un array care sa contina propozitiile“{friendName} are {age} ani.”
+`);
+const frase = person.friends.map((friend) => {
+  const { name: friendName, age: friendAge } = friend;
+  return `${friendName} are ${friendAge} ani.`;
+});
+console.log(frase);
+
+console.warn(`8.Folosind metoda map pe arrayul friends,
+returneaza un array care contine numele inversat al prietenilor pe fiecare pozitie (exemplu: Stevenson Steven)
+`);
+const reverseName = person.friends.map((friend) => {
+  const { name: friendName, surname: friendSurname } = friend;
+  return `${friendSurname} ${friendName}`;
+});
+console.log(reverseName);
+
+console.warn(`9.Folosind metoda map pe arrayul friends,
+ returneaza un array care contine pe fiecare pozitie diferenta dintre lungimea totala al numelui complet (fara spatii)
+  si varsta prietenului de pe iteratie
+`);
+const difference = person.friends.map((friend) => {
+  const { name: friendName, surname: friendSurname, age: friendAge } = friend;
+  const completeName = `${friendName}${friendSurname}`;
+  const difff = Math.abs(completeName.length - friendAge);
+  return difff;
+});
+console.log(difference);
+
+console.warn(`10.Folosind metoda map pe arrayul skills returneaza un array care contine diferenta dintre lungimea fiecarui skill si
+varsta prietenului
+`);
+const skillLengthAgeDiff = person.skills.map((skill) => {
+  const skillMinusAge = person.friends.map((friend) => {
+    const { age: friendAge } = friend;
+    return Math.abs(skill.length - friendAge);
+  });
+  return skillMinusAge;
+});
+
+console.log(skillLengthAgeDiff);
